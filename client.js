356ee0aece9b301bd53185db70b412459a2fda70
blackarmVR <honungsbjorn@protonmail.com>
2021-03-18 10:06:10 +0000

    fix

--- client.js
+++ client.js
@@ -6,15 +6,15 @@ function() {
             serverLink.data.getRooms = false;
             roomNameList = serverLink.data.roomNameList;
             exportData("roomNameList", roomNameList);
+            exportData("test", ["test"]);
         });
     }
     function exportData(variableName, data){
         var output = document.getElementById("test");
         output.innerHTML += "var " + variableName + " = ";
         output.innerHTML += JSON.stringify(data);
-        output.innerHTML += "\n";
+        output.innerHTML += ";\n";
     }
-    var output = document.getElementById("test");
     var serverLink = this;
     getData()
 }
